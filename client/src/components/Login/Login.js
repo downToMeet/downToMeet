89c564bcb0bc53d17cc530efa24eb50405630985
Timothy Rediehs <trediehs@g.ucla.edu>
2020-11-18 22:50:13 -0800

    Create basic log in page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Button, Container, Typography } from "@material-ui/core";
+import FacebookLogo from "./assets/FacebookLogo";
+import GoogleLogo from "./assets/GoogleLogo";
+
+const useStyles = makeStyles((theme) => ({
+  loginButton: {
+    margin: "1em",
+    backgroundColor: "#EEEEEE",
+    textTransform: "capitalize",
+    fontFamily: theme.fontFamily,
+    "&:hover": {
+      backgroundColor: "#CCCCCC",
+    },
+  },
+  loginBox: {
+    height: "calc(90vh - 40px)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+}));
 
 function Login() {
-  return <>Login</>;
+  const classes = useStyles();
+  return (
+    <Container maxWidth="sm" className={classes.loginBox}>
+      <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        style={{ marginBottom: "clamp(10px, 7vh, 44px)" }}
+      >
+        Sign in
+      </Typography>
+      <Box display="flex" justifyContent="center">
+        <Button
+          component="a"
+          href="http://localhost:9000/user/facebook/auth"
+          className={classes.loginButton}
+          startIcon={<FacebookLogo />}
+        >
+          Using Facebook
+        </Button>
+        <Button
+          component="a"
+          href="http://localhost:9000/user/google/auth"
+          className={classes.loginButton}
+          startIcon={<GoogleLogo />}
+        >
+          Using Google
+        </Button>
+      </Box>
+    </Container>
+  );
 }
 
 export default Login;
