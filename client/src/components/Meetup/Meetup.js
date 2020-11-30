6a1b6aff2d0f7b8ce46c9f41ad5792adceff5b4a
Linus Lam <linuslyt@gmail.com>
2020-11-30 13:35:09 +0800

    Rearrange content into Grids

--- client/src/components/Meetup/Meetup.js
+++ client/src/components/Meetup/Meetup.js
@@ -11,13 +11,14 @@ import {
   CardActions,
   Avatar,
   Button,
+  Grid,
 } from "@material-ui/core";
 import GroupAddIcon from "@material-ui/icons/GroupAdd";
-import makeStyles from "@material-ui/styles/makeStyles";
+import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import Paper from "@material-ui/core/Paper";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   // TODO: mobile scaling
   // TODO: finalize styles (font, color)
   root: {
@@ -27,8 +28,10 @@ const useStyles = makeStyles(() => ({
     alignItems: "center",
   },
   paper: {
-    padding: 25,
-    width: "75%",
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(4),
+    padding: theme.spacing(5),
+    width: "80%",
   },
   link: {
     textDecoration: "none",
@@ -40,16 +43,26 @@ const useStyles = makeStyles(() => ({
   },
   tag: {
     display: "inline",
+    margin: theme.spacing(0.25),
   },
   attendeeList: {
+    marginTop: theme.spacing(2),
     padding: 0,
     listStyle: "none",
   },
   attendee: {
-    display: "inline",
+    marginBottom: theme.spacing(2),
   },
   organizer: {
-    width: "75%",
+    marginTop: theme.spacing(2),
+    width: 500,
+  },
+  organizerBio: {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    display: "-webkit-box",
+    "-webkit-line-clamp": 3,
+    "-webkit-box-orient": "vertical",
   },
 }));
 function Meetup({ id }) {
@@ -60,7 +73,7 @@ function Meetup({ id }) {
   const [meetupType, setMeetupType] = useState("");
   const [meetupURL, setMeetupURL] = useState("");
   const [meetupLocation, setMeetupLocation] = useState(null);
-  const [groupCount, setGroupCount] = useState([2, 10]);
+  const [groupCount, setGroupCount] = useState([2, 18]);
   const [description, setDescription] = useState("");
   const [tags, setTags] = useState([]);
   const [attendees, setAttendees] = useState([]);
@@ -90,7 +103,18 @@ function Meetup({ id }) {
     groupCount: [2, 10],
     description: "Test meetup, do not join",
     tags: ["send", "help", "pls"],
-    attendees: ["jim", "joe", "bob", "foo", "bar"],
+    attendees: [
+      "jim",
+      "joe",
+      "bob",
+      "foo",
+      "bar",
+      "baz",
+      "idk",
+      "any",
+      "more",
+      "names",
+    ],
     organizer: "kim",
   };
 
@@ -98,6 +122,21 @@ function Meetup({ id }) {
     setData(mockData);
     // split into organizer, event, attendee data
   }, []);
+
+  const locale = "en-US";
+  const eventTimeOptions = {
+    hour: "numeric",
+    minute: "numeric",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    timeZoneName: "short",
+  };
+  const userDateOptions = {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
 
   const renderTags = (tagList) => {
     // TODO: convert to search link w/ tags
@@ -124,7 +163,17 @@ function Meetup({ id }) {
     return (
       <>
         {attendeeList.map((attendee) => (
-          <Avatar />
+          <Grid
+            item
+            container
+            direction="column"
+            xs={3}
+            alignItems="center"
+            className={classes.attendee}
+          >
+            <Avatar size="small" />
+            {attendee}
+          </Grid>
         ))}
       </>
     );
@@ -138,42 +187,99 @@ function Meetup({ id }) {
 
   return (
     <Box className={classes.root}>
-      <Typography className={classes.link} component={Link} to="/">
-        Return home
-      </Typography>
       <Paper className={classes.paper}>
-        <Typography variant="h1">{title}</Typography>
-        <Button startIcon={<GroupAddIcon />} onClick={handleJoinMeetup}>
-          Join Meetup
-        </Button>
-        {/* add share/link copy icon here? */}
-        <Typography>Time: {time.toString()}</Typography>
-        <Typography>Location: {meetupLocation || "Online"}</Typography>
-        <Typography>
-          Attendees: {`${attendees.length} out of ${groupCount[1]}`}
-        </Typography>
-        <Typography component="ul" className={classes.tagList}>
-          Tags: {renderTags(tags)}
-        </Typography>
-        <Card className={classes.organizer}>
-          <CardHeader
-            avatar={
-              <Avatar className={classes.organizerAvatar} src={profilePic} />
-            }
-            title={organizer}
-            subheader={`member since ${time.toString()}`}
-          />
-          <CardContent>
-            <Typography>About me</Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <Button>Contact</Button>
-          </CardActions>
-        </Card>
-        <Typography component="ul" className={classes.attendeeList}>
-          Attendees: <br />
-          {renderAttendees(attendees)}
-        </Typography>
+        <Grid container spacing={1}>
+          <Grid item>
+            <Typography className={classes.link} component={Link} to="/">
+              &lt; return home
+            </Typography>
+          </Grid>
+          <Grid item container>
+            <Grid item xs>
+              <Typography variant="h3">{title}</Typography>
+              {/* add share/link copy icon here? */}
+            </Grid>
+            <Grid
+              item
+              xs={2}
+              container
+              justify="center"
+              alignItems="flex-start"
+            >
+              <Button startIcon={<GroupAddIcon />} onClick={handleJoinMeetup}>
+                Join Meetup
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item container direction="column" spacing={1}>
+            <Grid item>
+              <Typography>
+                Time: {time.toLocaleString(locale, eventTimeOptions)}
+              </Typography>
+            </Grid>
+            <Grid item>
+              {/* TODO: Link to online event if joined? */}
+              <Typography>Location: {meetupLocation || "Online"}</Typography>
+            </Grid>
+            <Grid item>
+              <Typography>
+                Attendees:{" "}
+                {`${attendees.length} out of ${groupCount[1]} (min. ${groupCount[0]})`}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography component="ul" className={classes.tagList}>
+                Tags: {renderTags(tags)}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography>Organizer:</Typography>
+              <Card className={classes.organizer}>
+                <CardHeader
+                  avatar={
+                    <Avatar
+                      className={classes.organizerAvatar}
+                      src={profilePic}
+                    />
+                  }
+                  title={organizer}
+                  subheader={`member since ${time.toLocaleString(
+                    locale,
+                    userDateOptions
+                  )}`}
+                />
+                <CardContent>
+                  <Typography className={classes.organizerBio}>
+                    About Me: Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit, sed do eiusmod tempor incididunt ut labore et dolore
+                    magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <Button>Contact</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item container direction="column" spacing={1}>
+              <Grid item>
+                <Typography>Attendees: </Typography>
+              </Grid>
+              <Grid
+                item
+                container
+                justify="center"
+                className={classes.attendeeList}
+              >
+                {renderAttendees(attendees)}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </Paper>
     </Box>
   );
