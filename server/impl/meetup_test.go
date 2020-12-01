a3ac68744499e5e9c03303793e21b681be0307d4
Timothy Gu <timothygu99@gmail.com>
2020-12-01 14:46:18 -0500

    Various cleanups

--- server/impl/meetup_test.go
+++ server/impl/meetup_test.go
@@ -2,22 +2,22 @@ package impl_test
 
 import (
 	"fmt"
-	"github.com/go-openapi/strfmt"
-	"github.com/go-openapi/swag"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"go.timothygu.me/downtomeet/server/db"
-	"go.timothygu.me/downtomeet/server/impl"
-	"go.timothygu.me/downtomeet/server/models"
-	"go.timothygu.me/downtomeet/server/restapi/operations"
 	"math"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
+
+	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.timothygu.me/downtomeet/server/db"
+	"go.timothygu.me/downtomeet/server/impl"
+	"go.timothygu.me/downtomeet/server/models"
+	"go.timothygu.me/downtomeet/server/restapi/operations"
 )
-
-
 
 func TestGetMeetup(t *testing.T) {
 	const sessionName = "session"
@@ -40,15 +40,15 @@ func TestGetMeetup(t *testing.T) {
 	res := raw.(*operations.GetMeetupOK)
 	assert.Greaterf(t, len(res.Payload), 0, "I can't test anything if I there are no meetups in range")
 	for _, meetup := range res.Payload {
-		assert.LessOrEqual(t, math.Abs(*meetup.Location.Coordinates.Lat - params.Lat), params.Radius)
-		assert.LessOrEqual(t, math.Abs(*meetup.Location.Coordinates.Lon - params.Lon), params.Radius)
+		assert.LessOrEqual(t, math.Abs(*meetup.Location.Coordinates.Lat-params.Lat), params.Radius)
+		assert.LessOrEqual(t, math.Abs(*meetup.Location.Coordinates.Lon-params.Lon), params.Radius)
 	}
 }
 
 func TestGetMeetupID(t *testing.T) {
 	const sessionName = "session"
 	url := new(operations.GetMeetupIDURL)
-	url.ID = fmt.Sprintf("%v", TestMeetup.ID)
+	url.ID = TestMeetup.IDString()
 	req := httptest.NewRequest(http.MethodGet, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
 	require.NoError(t, err)
@@ -62,13 +62,13 @@ func TestGetMeetupID(t *testing.T) {
 
 	require.IsType(t, (*operations.GetMeetupIDOK)(nil), raw)
 	res := raw.(*operations.GetMeetupIDOK)
-	assert.Equal(t, fmt.Sprintf("%v", res.Payload.ID), fmt.Sprintf("%v", TestMeetup.ID))
+	assert.Equal(t, fmt.Sprint(res.Payload.ID), TestMeetup.IDString())
 }
 
 func TestGetMeetupIDNotFound(t *testing.T) {
 	const sessionName = "session"
 	url := new(operations.GetMeetupIDURL)
-	nonexistentUserID := fmt.Sprintf("%v", TestUser.ID + 100000)
+	nonexistentUserID := TestUser.IDString() + "00000"
 	url.ID = nonexistentUserID
 	req := httptest.NewRequest(http.MethodGet, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
@@ -77,7 +77,7 @@ func TestGetMeetupIDNotFound(t *testing.T) {
 
 	params := operations.GetMeetupIDParams{
 		HTTPRequest: req,
-		ID: nonexistentUserID,
+		ID:          nonexistentUserID,
 	}
 
 	raw := testImpl.GetMeetupID(params)
@@ -92,7 +92,7 @@ func TestPostMeetup(t *testing.T) {
 	url := new(operations.PostMeetupURL)
 	req := httptest.NewRequest(http.MethodPost, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
-	session.Values[impl.UserID] = fmt.Sprintf("%v", TestUser.ID)
+	session.Values[impl.UserID] = TestUser.IDString()
 	require.NoError(t, err)
 	req = req.WithContext(impl.WithSession(req.Context(), session))
 
@@ -129,7 +129,7 @@ func TestPostMeetupTags(t *testing.T) {
 	url := new(operations.PostMeetupURL)
 	req := httptest.NewRequest(http.MethodPost, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
-	session.Values[impl.UserID] = fmt.Sprintf("%v", TestUser.ID)
+	session.Values[impl.UserID] = TestUser.IDString()
 	require.NoError(t, err)
 	req = req.WithContext(impl.WithSession(req.Context(), session))
 
@@ -154,24 +154,23 @@ func TestPostMeetupTags(t *testing.T) {
 		},
 	}
 
-
 	raw := testImpl.PostMeetup(params, nil)
 
 	require.IsType(t, (*operations.PostMeetupCreated)(nil), raw)
 	res := raw.(*operations.PostMeetupCreated)
 	assert.Equal(t, res.Payload.Title, eventTitle)
 	meetup := db.Meetup{}
-	testImpl.DB().Preload("Tags").First(&meetup, fmt.Sprintf("%v", res.Payload.ID))
-	assert.Equalf(t, 2, len(meetup.Tags),"This meetup was created with two tags.")
+	testImpl.DB().Preload("Tags").First(&meetup, fmt.Sprint(res.Payload.ID))
+	assert.Equalf(t, 2, len(meetup.Tags), "This meetup was created with two tags.")
 }
 
 func TestPostMeetupBadUser(t *testing.T) {
 	const sessionName = "session"
-	const nonexistentUserId = 10000000
+	const nonexistentUserId = "10000000"
 	url := new(operations.PostMeetupURL)
 	req := httptest.NewRequest(http.MethodPost, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
-	session.Values[impl.UserID] = fmt.Sprintf("%v", nonexistentUserId)
+	session.Values[impl.UserID] = nonexistentUserId
 	require.NoError(t, err)
 	req = req.WithContext(impl.WithSession(req.Context(), session))
 
@@ -205,16 +204,16 @@ func TestPatchMeetup(t *testing.T) {
 	const sessionName = "session"
 	const newDescription = "UWU"
 	url := new(operations.PatchMeetupIDURL)
-	url.ID = fmt.Sprintf("%v", TestMeetup.ID)
+	url.ID = TestMeetup.IDString()
 	req := httptest.NewRequest(http.MethodPatch, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
-	session.Values[impl.UserID] = fmt.Sprintf("%v", TestUser.ID)
+	session.Values[impl.UserID] = TestUser.IDString()
 	require.NoError(t, err)
 	req = req.WithContext(impl.WithSession(req.Context(), session))
 
 	params := operations.PatchMeetupIDParams{
 		HTTPRequest: req,
-		ID: fmt.Sprintf("%v", TestMeetup.ID),
+		ID:          TestMeetup.IDString(),
 		Meetup: &models.MeetupRequestBody{
 			Description: newDescription,
 			Location: &models.Location{
@@ -246,13 +245,13 @@ func TestPatchMeetupNotFound(t *testing.T) {
 	url.ID = nonexistentMeetupID
 	req := httptest.NewRequest(http.MethodPatch, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
-	session.Values[impl.UserID] = fmt.Sprintf("%v", TestUser.ID)
+	session.Values[impl.UserID] = TestUser.IDString()
 	require.NoError(t, err)
 	req = req.WithContext(impl.WithSession(req.Context(), session))
 
 	params := operations.PatchMeetupIDParams{
 		HTTPRequest: req,
-		ID: nonexistentMeetupID,
+		ID:          nonexistentMeetupID,
 		Meetup: &models.MeetupRequestBody{
 			Description: newDescription,
 			Location: &models.Location{
@@ -278,16 +277,16 @@ func TestPatchMeetupForbidden(t *testing.T) {
 	const sessionName = "session"
 	const newDescription = "UWU"
 	url := new(operations.PatchMeetupIDURL)
-	url.ID = fmt.Sprintf("%v", TestMeetup.ID)
+	url.ID = TestMeetup.IDString()
 	req := httptest.NewRequest(http.MethodPatch, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
-	session.Values[impl.UserID] = fmt.Sprintf("%v", FakeUser.ID)
+	session.Values[impl.UserID] = FakeUser.IDString()
 	require.NoError(t, err)
 	req = req.WithContext(impl.WithSession(req.Context(), session))
 
 	params := operations.PatchMeetupIDParams{
 		HTTPRequest: req,
-		ID: fmt.Sprintf("%v", TestMeetup.ID),
+		ID:          TestMeetup.IDString(),
 		Meetup: &models.MeetupRequestBody{
 			Description: newDescription,
 			Location: &models.Location{
@@ -312,29 +311,29 @@ func TestPatchMeetupForbidden(t *testing.T) {
 func TestDeleteMeetup(t *testing.T) {
 	// Create a meetup to delete
 	doomedMeetup := db.Meetup{
-		Title:             "To Be Deleted",
-		Time:              time.Time{},
-		Description:       "",
-		MaxCapacity:       2,
-		MinCapacity:       1,
-		Owner:             TestUser.ID,
-		Location:          db.MeetupLocation{},
-		Cancelled:         false,
+		Title:       "To Be Deleted",
+		Time:        time.Time{},
+		Description: "",
+		MaxCapacity: 2,
+		MinCapacity: 1,
+		Owner:       TestUser.ID,
+		Location:    db.MeetupLocation{},
+		Cancelled:   false,
 	}
 	testImpl.DB().Create(&doomedMeetup)
 
 	const sessionName = "session"
 	url := new(operations.DeleteMeetupIDURL)
-	url.ID = fmt.Sprintf("%v", TestMeetup.ID)
+	url.ID = TestMeetup.IDString()
 	req := httptest.NewRequest(http.MethodDelete, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
-	session.Values[impl.UserID] = fmt.Sprintf("%v", TestUser.ID)
+	session.Values[impl.UserID] = TestUser.IDString()
 	require.NoError(t, err)
 	req = req.WithContext(impl.WithSession(req.Context(), session))
 
 	params := operations.DeleteMeetupIDParams{
 		HTTPRequest: req,
-		ID: fmt.Sprintf("%v", doomedMeetup.ID),
+		ID:          doomedMeetup.IDString(),
 	}
 
 	raw := testImpl.DeleteMeetupID(params, nil)
@@ -343,19 +342,19 @@ func TestDeleteMeetup(t *testing.T) {
 }
 
 func TestDeleteMeetupNotFound(t *testing.T) {
-	const nonExistentMeetupID = 1000000
+	const nonExistentMeetupID = "1000000"
 	const sessionName = "session"
 	url := new(operations.DeleteMeetupIDURL)
-	url.ID = fmt.Sprintf("%v", nonExistentMeetupID)
+	url.ID = nonExistentMeetupID
 	req := httptest.NewRequest(http.MethodDelete, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
-	session.Values[impl.UserID] = fmt.Sprintf("%v", TestUser.ID)
+	session.Values[impl.UserID] = TestUser.IDString()
 	require.NoError(t, err)
 	req = req.WithContext(impl.WithSession(req.Context(), session))
 
 	params := operations.DeleteMeetupIDParams{
 		HTTPRequest: req,
-		ID: fmt.Sprintf("%v", nonExistentMeetupID),
+		ID:          nonExistentMeetupID,
 	}
 
 	raw := testImpl.DeleteMeetupID(params, nil)
@@ -366,28 +365,28 @@ func TestDeleteMeetupNotFound(t *testing.T) {
 func TestDeleteMeetupForbidden(t *testing.T) {
 	// Create a meetup to delete
 	doomedMeetup := db.Meetup{
-		Title:             "To Be Deleted",
-		Time:              time.Time{},
-		Description:       "",
-		MaxCapacity:       2,
-		MinCapacity:       1,
-		Owner:             TestUser.ID,
-		Location:          db.MeetupLocation{},
-		Cancelled:         false,
+		Title:       "To Be Deleted",
+		Time:        time.Time{},
+		Description: "",
+		MaxCapacity: 2,
+		MinCapacity: 1,
+		Owner:       TestUser.ID,
+		Location:    db.MeetupLocation{},
+		Cancelled:   false,
 	}
 	testImpl.DB().Create(&doomedMeetup)
 	const sessionName = "session"
 	url := new(operations.DeleteMeetupIDURL)
-	url.ID = fmt.Sprintf("%v", TestMeetup.ID)
+	url.ID = TestMeetup.IDString()
 	req := httptest.NewRequest(http.MethodDelete, url.String(), nil)
 	session, err := testImpl.SessionStore().New(req, sessionName)
-	session.Values[impl.UserID] = fmt.Sprintf("%v", FakeUser.ID)
+	session.Values[impl.UserID] = FakeUser.IDString()
 	require.NoError(t, err)
 	req = req.WithContext(impl.WithSession(req.Context(), session))
 
 	params := operations.DeleteMeetupIDParams{
 		HTTPRequest: req,
-		ID: fmt.Sprintf("%v", doomedMeetup.ID),
+		ID:          doomedMeetup.IDString(),
 	}
 
 	raw := testImpl.DeleteMeetupID(params, nil)
