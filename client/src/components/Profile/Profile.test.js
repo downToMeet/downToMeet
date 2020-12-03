554c1d340251bd4ffc5a56f28cdab3758a309f69
Timothy Gu <timothygu99@gmail.com>
2020-12-03 00:48:37 -0500

    Add login test

--- client/src/components/Profile/Profile.test.js
+++ client/src/components/Profile/Profile.test.js
@@ -2,23 +2,9 @@
 
 import React from "react";
 import { render } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import PropTypes from "prop-types";
-import store from "../../app/store";
+
 import Profile from "./Profile";
-
-function Wrapper({ children }) {
-  return (
-    <Provider store={store}>
-      <MemoryRouter>{children}</MemoryRouter>
-    </Provider>
-  );
-}
-
-Wrapper.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+import { Wrapper } from "../test";
 
 test("renders own profile", () => {
   const user = {
